1e1912c6c1b4b7dc55d90b4b78d27df816fc2bab
Антон Аврамчук <avramchuk.a@promail366.com>
2025-12-27 20:16:09 +0200

    add application generator

--- src/Cheetah.Core/Modules/ICrmModule.cs
+++ src/Cheetah.Core/Modules/ICrmModule.cs
@@ -1,6 +1,3 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
 namespace Cheetah.Core.Modules;
 
 /// <summary>
@@ -8,38 +5,5 @@ namespace Cheetah.Core.Modules;
 /// </summary>
 public interface ICrmModule
 {
-    /// <summary>
-    /// Unique name of the module
-    /// </summary>
-    string? Name { get; }
     
-    /// <summary>
-    /// Called before ConfigureServices - for early initialization
-    /// </summary>
-    void PreConfigure() { }
-    
-    /// <summary>
-    /// Configure services for dependency injection
-    /// </summary>
-    void ConfigureServices(IServiceCollection services, IConfiguration configuration);
-    
-    /// <summary>
-    /// Called after ConfigureServices - for late configuration
-    /// </summary>
-    void PostConfigure() { }
-    
-    /// <summary>
-    /// Called before module initialization
-    /// </summary>
-    ValueTask OnPreInitializeAsync(IServiceProvider services) => ValueTask.CompletedTask;
-    
-    /// <summary>
-    /// Main module initialization (e.g., run migrations)
-    /// </summary>
-    ValueTask OnInitializeAsync(IServiceProvider services) => ValueTask.CompletedTask;
-    
-    /// <summary>
-    /// Called after module initialization
-    /// </summary>
-    ValueTask OnPostInitializeAsync(IServiceProvider services) => ValueTask.CompletedTask;
 }
---
+++ src/Cheetah.Generators.Core/Constants.cs
@@ -0,0 +1,17 @@
+using Cheetah.Core;
+using Cheetah.Core.DependencyInjection;
+using Cheetah.Core.Modules;
+
+namespace Cheetah.Generators.Core;
+
+public static class Constants
+{
+    public static string? BootstrapperAttributeName = typeof(BootstrapperAttribute).FullName;
+
+    public static string? ModuleTypeName = typeof(ICrmModule).FullName;
+
+    public static string? ExportAttributeName = typeof(ExportAttribute).FullName;
+
+
+    public static string? DependsOnAttributeName = typeof(DependsOnAttribute).FullName;
+}
