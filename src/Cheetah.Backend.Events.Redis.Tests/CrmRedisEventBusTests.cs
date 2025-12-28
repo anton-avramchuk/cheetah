4a1c7a796b1cd6a278aaa0d83e2347a75d9c344b
Антон Аврамчук <avramchuk.a@promail366.com>
2025-12-28 09:49:25 +0200

    tests

--- src/Cheetah.Backend.Events.Redis.Tests/CrmRedisEventBusTests.cs
+++ src/Cheetah.Backend.Events.Redis.Tests/CrmRedisEventBusTests.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Moq;
 
-namespace Cheetah.Backend.Redis.Tests;
+namespace Cheetah.Backend.Events.Redis.Tests;
 
 public class CrmRedisEventBusTests
 {
