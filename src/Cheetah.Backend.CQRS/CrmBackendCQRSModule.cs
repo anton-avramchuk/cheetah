1acf3892164c97c20f8b9ee454c8bdbeedd57779
Антон Аврамчук <avramchuk.a@promail366.com>
2025-12-28 09:44:07 +0200

    add cqrs module for backend

---
+++ src/Cheetah.Backend.CQRS/CrmBackendCQRSModule.cs
@@ -0,0 +1,13 @@
+﻿using Cheetah.Core.CQRS;
+using Cheetah.Core.Modularity;
+
+namespace Cheetah.Backend.CQRS;
+
+[DependsOn(typeof(CrmCQRSCoreModule))]
+public partial class CrmBackendCQRSModule:CrmModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        RegisterServices(context.Services);
+    }
+}
