108365a5e1a8a11331638c13abdb07cd4d0ee400
Антон Аврамчук <avramchuk.a@promail366.com>
2026-01-06 10:31:59 +0200

    refactoring

---
+++ src/Cheetah.Backend.IdentityCore.Contracts/CrmIdentityCoreContractsModule.cs
@@ -0,0 +1,9 @@
+﻿using Cheetah.Backend.IdentityCore.Shared;
+using Cheetah.Core.Modularity;
+
+namespace Cheetah.Backend.IdentityCore.Contracts;
+
+[DependsOn(typeof(CrmIdentityCoreSharedModule))]
+public class CrmIdentityCoreContractsModule : CrmModule
+{
+}
