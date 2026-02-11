5ad11a78b39261ba9ae999729be0675294aea10d
Антон Аврамчук <avramchuk.a@promail366.com>
2026-02-11 13:23:33 +0200

    add migrations

---
+++ src/Modules/Candidates/Candidates/Crm.Candidates.DataAccess/Migrations/20260211112014_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Crm.Candidates.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "candidates");
+
+            migrationBuilder.CreateTable(
+                name: "Candidate",
+                schema: "candidates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidate", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidate_Name",
+                schema: "candidates",
+                table: "Candidate",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidate",
+                schema: "candidates");
+        }
+    }
+}
