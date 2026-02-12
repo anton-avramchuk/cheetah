4c734e43d98b7d403d981bf8868cf7ddb49345aa
Антон Аврамчук <avramchuk.a@promail366.com>
2026-02-12 12:58:25 +0200

    add recruitment frontend module

---
+++ src/Modules/Recruitment/Crm.Recruitment.Frontend/Models/VacancyGridViewModel.cs
@@ -0,0 +1,22 @@
+using Cheetah.Blazor.Components.Crud;
+using Crm.Recruitment.Contracts.Response;
+
+namespace Crm.Recruitment.Frontend.Models;
+
+public sealed class VacancyGridViewModel : IGridViewModel
+{
+    public Guid Id { get; init; }
+
+    [GridColumn(DisplayName = "Name", Order = 1)]
+    public string Name { get; init; } = "";
+
+    [GridColumn(DisplayName = "Description", Order = 2)]
+    public string? Description { get; init; }
+
+    public static VacancyGridViewModel FromResponse(VacancyViewModel response) => new()
+    {
+        Id = response.Id,
+        Name = response.Name,
+        Description = response.Description
+    };
+}
