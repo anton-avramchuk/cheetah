188654d8237947443417aaf4bafbad17fcd6fb16
Антон Аврамчук <avramchuk.a@promail366.com>
2026-02-20 17:40:47 +0200

    proxy config. add to docker

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -3,3 +3,4 @@ CREATE DATABASE cheetah_features;
 CREATE DATABASE cheetah_recruitment;
 CREATE DATABASE cheetah_candidates;
 CREATE DATABASE cheetah_vacancy_tasks;
+CREATE DATABASE cheetah_identity;
